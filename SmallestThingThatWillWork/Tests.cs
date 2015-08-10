530b4b21888cce80359ada6e7856f7cf7ebe3a96
Dave Craft <CraftyFella@gmail.com>
2015-08-10 14:08:49 +0100

    Dave First Commit

---
+++ SmallestThingThatWillWork/Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SmallestThingThatWillWork
+{
+    public class Tests
+    {
+        [Test]
+        public void A_failing_test()
+        {
+            Assert.Fail("I'm failing.. oh nose!");
+        }
+
+        [Test]
+        public void A_passing_test()
+        {
+            Assert.Pass("Weeee");
+        }
+
+        [Test]
+        public void A_warning_test()
+        {
+            Assume.That(false, "Warning..");
+        }
+    }
+}
